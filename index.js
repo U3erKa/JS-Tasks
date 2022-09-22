630a2d841cb7584263eba85e703b06b9bad5a496
Viktor Stepanov <vitya.zap@gmail.com>
2022-09-22 19:22:13 +0300

    Use error checkers

--- index.js
+++ index.js
@@ -13,16 +13,36 @@ class RangeValidator {
   get range() {
     return this.#array(this._from, this._to);
   }
+
   set from(from) {
+    if (typeof from !== 'number' || isNaN(from)) {
+      throw new TypeError('from must be a number');
+    }
+    if (!Number.isInteger(from)) {
+      throw new RangeError('from must be integer');
+    }
+    if (from > this._to) {
+      throw new RangeError('from must not be higher than to');
+    }
     this._from = from;
   }
   set to(to) {
+    if (typeof to !== 'number' || isNaN(to)) {
+      throw new TypeError('to must be a number');
+    }
+    if (!Number.isInteger(to)) {
+      throw new RangeError('to must be integer');
+    }
+    if (this._from > to) {
+      throw new RangeError('to must not be lower than from');
+    }
     this._to = to;
   }
-  set array(array) {
-    array(this._from, this._to);
-  }
+
   #array(from, to) {
+    if (this._from > this._to) {
+      throw new RangeError('from must not be higher than to');
+    }
     let array = [];
     for (let i = 0; i <= to - from; i++) {
       array[i] = i + from;
@@ -30,6 +50,13 @@ class RangeValidator {
     return array;
   }
   validate(number) {
+    if (typeof number !== 'number' || isNaN(number)) {
+      throw new TypeError('number must be a number');
+    }
+    if (!Number.isInteger(number)) {
+      throw new RangeError('number must be integer');
+    }
+
     for (let i = 0; i < this.range.length; i++) {
       if (this.range[i] === number) {
         return true;
